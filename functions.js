4251c48a528d5b8784fc937d7177801212578bc2
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-06 15:39:36 -0700

    created functions file, operator functions

---
+++ functions.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1 + num2
+} // adds, returns result
+
+function subtract(num1, num2) {
+    return num1 - num2
+} // subtracts, returns result
+
+function multiply(num1, num2) {
+    return num1 * num2
+} // multiplys, returns result
+
+function divide(num1, num2) {
+    return num1 / num2
+} // divides, returns result
+
+
+function operation(num1, num2, operator) {
+    let result;
+
+    switch(operator) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+    }
+}
